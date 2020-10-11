dc2cec83a932430e1062754a5fcc9e91d1262a2d
Holger Schmitz <holger@notjustphysics.com>
2020-10-11 16:20:15 +0100

    First tutorial

---
+++ basics/01_print_input.py
@@ -0,0 +1,45 @@
+
+# The hash symbol starts a comment
+# Programs contain comments to explain the code to other people
+# They don't actually do anything
+
+# The print statement writes out a string to the user
+print("Hello, this is a simple program")
+
+# Strings are bits of text surrounded by single ' or double quotes ".
+
+# You can store a string in a variable, here 'information' is the name of the variable
+information = 'A string stored in a variable'
+
+# This is called 'assigning a value' to a variable
+
+# Here we are printing the contents of the variable
+print(information)
+
+# The input statement prompts the user for some input
+name = input("What is your name? ")
+
+# You can use the '+' symbol to add strings.
+# This is calles concatenation
+message = "Welcome "+name+"!"
+print(message)
+
+# Now, try it out yourself! 
+# Remember to use the print statement to see what the value of a variable is.
+# 
+# 1) Create your own string variables and assign a value to them.
+#
+# 2) What happens when you assign a different value that you have already used?
+# 
+# 3) Can you use the same variable mutliple times when adding strings with +
+#
+# 4) What happens when you mutliply a string with a number like this
+#    "a rose is"*5
+# 
+# 5) Does the result of the last task look good? How can you make it more readable?
+# 
+# 6) Ask the user for a word and then print a message where the word is repeated three times.
+#
+# 7) Just like in maths, you can combine operators * and + with brackets ()
+#    Try something like 
+#    (message + " ")*3
