0a1f6a8de2f10815198863423c1b2184ce4cfedb
Holger Schmitz <holger@notjustphysics.com>
2023-04-10 15:14:00 +0100

    Moving a rectangle on the board

--- pygame/basics/lines.py
+++ pygame/basics/lines.py
@@ -4,18 +4,46 @@ import pygame
 
 pygame.init()
 running = True
-count = 1
+xvel = 1
+yvel = 0
+
+xpos = 25
+ypos = 25
 
 screen = pygame.display.set_mode((500, 500))
 
+MOVEEVENT = pygame.USEREVENT+1
+pygame.time.set_timer(MOVEEVENT, 120)
+
 while running:
-    count = (count+1) % 500
 
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
         print('Quitting!')
         running = False
+    elif event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_RIGHT:
+            xvel = 1
+            yvel = 0
+        elif event.key == pygame.K_LEFT:
+            xvel = -1
+            yvel = 0
+        elif event.key == pygame.K_UP:
+            xvel = 0
+            yvel = -1
+        elif event.key == pygame.K_DOWN:
+            xvel = 0
+            yvel = 1
+    elif event.type == MOVEEVENT:
+        xpos = xpos + xvel
+        ypos = ypos + yvel
+
     screen.fill((255, 0, 0))
-    pygame.draw.line(screen, (0, 0, 0), (250, count), (250 + 200, count))
+
+
+    xcoord = 10*xpos
+    ycoord = 10*ypos
+
+    pygame.draw.rect(screen, (0, 0, 0), (xcoord, ycoord, 10, 10))
     pygame.display.flip()
-    print(count)
+    print(f"{xpos}, {ypos}")
