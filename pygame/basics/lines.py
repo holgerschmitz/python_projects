50aed9ee24aa2849e2b766c6b99cdd06c66f91fc
Holger Schmitz <holger@notjustphysics.com>
2023-05-01 18:13:27 +0100

    Added food and a tail

--- pygame/basics/lines.py
+++ pygame/basics/lines.py
@@ -1,6 +1,7 @@
 #!python3
 
 import pygame
+import random
 
 pygame.init()
 running = True
@@ -13,10 +14,23 @@ ypos = 25
 screen = pygame.display.set_mode((500, 500))
 
 MOVEEVENT = pygame.USEREVENT+1
+FOODEVENT = pygame.USEREVENT+2
 pygame.time.set_timer(MOVEEVENT, 120)
+pygame.time.set_timer(FOODEVENT, 1000)
+
+# area = []
+# for i in range(100):
+#     row = []
+#     for j in range(100):
+#         row.append['']
+#     area.append(row)
+
+haveFood = False
+foodPos = None
+
+tail = []
 
 while running:
-
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
         print('Quitting!')
@@ -35,14 +49,30 @@ while running:
             xvel = 0
             yvel = 1
     elif event.type == MOVEEVENT:
+        if haveFood and xpos==foodPos[0] and ypos==foodPos[1]:
+            haveFood = False
+            tail.insert(0, [0,0])
+
+        tail.append([xpos, ypos])
+        tail.pop(0)
         xpos = xpos + xvel
         ypos = ypos + yvel
+    elif event.type == FOODEVENT:
+        if not haveFood:
+            foodPos = [random.randrange(50), random.randrange(50)]
+            haveFood = True
 
-    screen.fill((255, 0, 0))
+    screen.fill((255, 255, 255))
 
 
     xcoord = 10*xpos
     ycoord = 10*ypos
+
+    if haveFood:
+        pygame.draw.rect(screen, (255, 0, 0), (foodPos[0]*10, foodPos[1]*10, 10, 10))
+
+    for tailPos in tail:
+        pygame.draw.rect(screen, (80, 80, 80), (tailPos[0]*10, tailPos[1]*10, 10, 10))
 
     pygame.draw.rect(screen, (0, 0, 0), (xcoord, ycoord, 10, 10))
     pygame.display.flip()
